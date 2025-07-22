4c0abbaca303580a646a863097e9186131356cf4
sunilach12 <sunilacharya338@gmail.com>
2025-07-23 00:58:51 +0545

    Update first.js
    update js

--- first.js
+++ first.js
@@ -1,1 +1,9 @@
+function download(file) {
+  const link = document.createElement('a');
+  link.href = file;
+  link.download = file;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
  
