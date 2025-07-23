6d43e02af1dab1af4ffa69d8bf27146d8f0bf9dd
sunilach12 <sunilacharya338@gmail.com>
2025-07-23 15:23:21 +0545

    Update first.js
    done

--- first.js
+++ first.js
@@ -1,17 +1,21 @@
 //Audio 1
 function playAudio() {
   const player = document.getElementById('audioPlayer');
-   const player = document.getElementById('audioPlayer1');
-   const player = document.getElementById('audioPlayer2');
+   const player1 = document.getElementById('audioPlayer1');
+   const player2 = document.getElementById('audioPlayer2');
   player.play();
+  player1.play();
+  player2.play();
 }
 
 function pauseAudio() {
   const player = document.getElementById('audioPlayer');
-  const player = document.getElementById('audioPlayer1');
-  const player = document.getElementById('audioPlayer2');
+  const player1 = document.getElementById('audioPlayer1');
+  const player2 = document.getElementById('audioPlayer2');
   player.pause();
-
+  player1.pause();
+  player2.pause();
+}
 
 function download(file) {
   const link = document.createElement('a');
