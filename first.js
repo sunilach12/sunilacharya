bbfd5fcf54e57a81e79a74bebddb340bc95ecda3
sunilach12 <sunilacharya338@gmail.com>
2025-07-23 15:20:13 +0545

    Update first.js
    done

--- first.js
+++ first.js
@@ -1,36 +1,16 @@
 //Audio 1
 function playAudio() {
   const player = document.getElementById('audioPlayer');
+   const player = document.getElementById('audioPlayer1');
+   const player = document.getElementById('audioPlayer2');
   player.play();
 }
 
 function pauseAudio() {
   const player = document.getElementById('audioPlayer');
-  player.pause();
-
-  //Audio 2
-}
-function playAudio() {
   const player = document.getElementById('audioPlayer1');
-  player.play();
-}
-
-function pauseAudio() {
-  const player = document.getElementById('audioPlayer1');
-  player.pause();
-}
-//Audio 3
-
-function playAudio() {
-  const player = document.getElementById('audioPlayer2');
-  player.play();
-}
-
-function pauseAudio() {
   const player = document.getElementById('audioPlayer2');
   player.pause();
-}
-
 
 
 function download(file) {
