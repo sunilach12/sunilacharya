2f502854d0a57085e1fc2a208a2c79767c7aea6c
sunilach12 <sunilacharya338@gmail.com>
2025-07-23 16:04:18 +0545

    Update first.js
    update 5

--- first.js
+++ first.js
@@ -1,11 +1,19 @@
- 
 
-function download(file) {
-  const link = document.createElement('a');
-  link.href = file;
-  link.download = file;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-} 
- 
+  function playAudio(playerId) {
+    const audio = document.getElementById(playerId);
+    audio.play();
+  }
+
+  function pauseAudio(playerId) {
+    const audio = document.getElementById(playerId);
+    audio.pause();
+  }
+
+  function download(fileName) {
+    const link = document.createElement("a");
+    link.href = fileName;
+    link.download = '';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
