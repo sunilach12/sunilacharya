7c6ab25a67a161af753b664bb7ac3be2ee786463
sunilach12 <sunilacharya338@gmail.com>
2026-01-08 18:57:19 +0545

    Update auth.js

--- auth.js
+++ auth.js
@@ -12,3 +12,15 @@ function logout() {
   showToast("You have logged out");
   setTimeout(() => location.href = "login.html", 1200);
 }
+function showToast(message) {
+  const toast = document.createElement("div");
+  toast.className = "toast";
+  toast.innerText = message;
+  document.body.appendChild(toast);
+
+  setTimeout(() => toast.classList.add("show"), 50);
+  setTimeout(() => {
+    toast.classList.remove("show");
+    setTimeout(() => toast.remove(), 300);
+  }, 2500);
+}
