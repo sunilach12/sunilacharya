a87569f6f21f51cd7ad8c4ee48cfc1afc4d0149f
sunilach12 <sunilacharya338@gmail.com>
2026-01-08 19:20:40 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-function showToast(message) {
+function showToast(message){
   const toast = document.createElement("div");
   toast.className = "toast";
   toast.innerText = message;
   document.body.appendChild(toast);
-
-  setTimeout(() => toast.classList.add("show"), 50);
-  setTimeout(() => {
-    toast.classList.remove("show");
-    setTimeout(() => toast.remove(), 300);
-  }, 2500);
+  setTimeout(()=>toast.classList.add("show"),50);
+  setTimeout(()=>{toast.classList.remove("show"); setTimeout(()=>toast.remove(),300);},2500);
 }
