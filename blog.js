f1581733398fa21452b81f679ad0f90c370ab8c2
sunilach12 <sunilacharya338@gmail.com>
2026-01-08 20:07:11 +0545

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,14 +1,50 @@
-function postBlog(title, text, image) {
-  let blogs = JSON.parse(localStorage.getItem("blogs")) || [];
-  blogs.push({
-    id: Date.now(),
-    title,
-    text,
-    image,
-    likes: 0,
-    comments: [],
-    author: getUser().username,
-    locked: false
+const blogForm = document.getElementById("blogForm");
+const blogsContainer = document.getElementById("blogsContainer");
+
+function renderBlogs(){
+  blogsContainer.innerHTML = "";
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  const blogs = currentUser.blogs || [];
+  blogs.forEach((b,i)=>{
+    const div = document.createElement("div");
+    div.className = "blog-card";
+    div.innerHTML = `
+      <h3>${b.title}</h3>
+      <p>${b.content}</p>
+      ${b.image ? `<img src="${b.image}" class="blog-img">` : ""}
+      <small>Posted by you</small>
+    `;
+    blogsContainer.appendChild(div);
   });
-  localStorage.setItem("blogs", JSON.stringify(blogs));
 }
+
+blogForm.addEventListener("submit", e=>{
+  e.preventDefault();
+  const title = document.getElementById("blogTitle").value;
+  const content = document.getElementById("blogContent").value;
+  const imageFile = document.getElementById("blogImage")?.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function(){
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    if(!currentUser.blogs) currentUser.blogs = [];
+    currentUser.blogs.push({
+      title,
+      content,
+      image: reader.result || null
+    });
+    localStorage.setItem("currentUser", JSON.stringify(currentUser));
+    blogForm.reset();
+    renderBlogs();
+    showNotification("Blog posted!");
+  }
+
+  if(imageFile){
+    reader.readAsDataURL(imageFile);
+  } else {
+    reader.onload();
+  }
+});
+
+// Render on page load
+renderBlogs();
